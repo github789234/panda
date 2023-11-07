2be06c368c2ea75111d834de427e150f80d5bd34
vbifhs <100491926+vbifhs@users.noreply.github.com>
2023-11-07 15:16:26 -0800

    uncommented bit declarration

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -152,7 +152,7 @@ static int toyota_rx_hook(CANPacket_t *to_push) {
 
     // most cars have brake_pressed on 0x226, corolla and rav4 on 0x224
     if (((addr == 0x224) && toyota_alt_brake) || ((addr == 0x226) && !toyota_alt_brake)) {
-      //uint8_t bit = (addr == 0x224) ? 5U : 37U;
+      uint8_t bit = (addr == 0x224) ? 5U : 37U;
       brake_pressed = GET_BIT(to_push, bit) != 0U;
     }
 
@@ -169,7 +169,7 @@ static int toyota_rx_hook(CANPacket_t *to_push) {
 
     generic_rx_checks((addr == 0x180) || (addr == 0x280) );
   }
-  
+
   return valid;
 }
 
